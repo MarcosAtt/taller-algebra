7649920e4e5c20bb7937fe4a52f22d41ec75e9e0
Marcos Attwell <marcos.attwell@gmail.com>
2021-09-08 23:13:50 -0300

    Complete los ejercicios de tarea y agrege ejercicios extra de la clase de hoy.

--- Clase04.hs
+++ Clase04.hs
@@ -1,18 +1,15 @@
-import Clase03(factorial)
-sumatoriaN :: Int -> Int
-sumatoriaN 0 = 0
-sumatoriaN n = n + sumatoriaN (n-1)
+import Clase03(factorial, esPar)
 
-sumatoriaN' :: Int -> Int
-sumatoriaN' n = n*(n+1) `div` 2
+sumatoria :: Int -> Int
+sumatoria 0 = 0
+sumatoria n = n + sumatoria (n-1)
+
+sumatoria' :: Int -> Int
+sumatoria' n = n*(n+1) `div` 2
 
 f1 :: Int -> Int
 f1 0 = 0
 f1 n = 2^n + f1(n-1)
-
-sumaQaLaN :: Int -> Int -> Int
-sumaQaLaN 0 _ = 0
-sumaQaLaN n q = q^n + sumaQaLaN (n-1) q
 
 f3 :: Int -> Int -> Int
 f3 n q = sumaQaLaN (2*n) q
@@ -46,5 +43,66 @@ sumaPotencias q n m = sumaPotencias q n (m-1) + q^m*(f2 n q)
 
 sumaRacionales :: Int -> Int -> Float
 sumaRacionales n 0 = 0
-sumaRacionales n m = (sumaRacionales n (m-1)) + (fromIntegral (sumatoriaN n)) / (fromIntegral m)
+sumaRacionales n m = (sumaRacionales n (m-1)) + (fromIntegral (sumatoria n)) / (fromIntegral m)
 {-Conclusion: cuando hay que sumar algo, escribir todos los terminos de la suma y buscar la invocacion de la misma funcion que estoy definiendo pero con valores mas cercanos al caso base-}
+
+sumaQaLaN :: Int -> Int -> Int
+sumaQaLaN 0 _ = 0
+sumaQaLaN n q = q^n + sumaQaLaN (n-1) q
+
+g1 :: Int -> Int -> Int
+g1 i n  | n == i = i^i
+        | otherwise = i^n + g1 i (n-1)
+
+sumatoriaExp :: Int -> Int -> Int
+sumatoriaExp 0 m = 0
+sumatoriaExp n m = n^m + sumatoriaExp (n-1) m
+
+g2 :: Int -> Int
+g2 1 = 1
+g2 n = sumaQaLaN n n + sumatoriaExp (n-1) n + g2 (n-1)
+
+g3 :: Int -> Int
+g3 0 = 0
+g3 n | esPar n = 2^n + g3 (n-2)
+     | otherwise = g3 (n-1)
+-- Suma todos los naturales <= n con digitos iguales.
+7
+Implementar una función que dado un n, sume todos los números naturales menores o
+iguales que n que tengan todos los dı́gitos iguales.
+sumaDigitosIguales :: Int -> Int
+sumaDigitosIguales | n == 0 = 0
+                   | n < 10 = n + sumaDigitosIguales (n-1)
+                   | 
+
+-- 1 Cumple propiedad de gauss.
+
+sumasGauss :: Int -> Bool
+sumasGauss n = sumatoria n == sumatoria' n
+
+-- 2 Suma de cuadrados por constantes.
+
+sumaCuadrados :: Int -> Int
+sumaCuadrados 0 = 0
+sumaCuadrados n = n^2 + sumaCuadrados (n-1)
+
+sumaConstante :: Int -> Int
+sumaConstante 0 = 0
+sumaConstante n = 7* n^2 + sumaConstante (n-1)
+
+constantesSumas :: Int -> Bool
+constantesSumas n = (sumaConstante (n)) == (7 * sumaCuadrados n)
+
+-- 3 Producto de sumas y sumas de productos
+
+productoSumas :: Int -> Int -> Int
+productoSumas n 1 = columna n 1
+productoSumas n m = productoSumas n (m-1) + columna n m
+
+columna :: Int -> Int -> Int
+columna 0 m = 0
+columna n m = columna (n-1) m + (2^n * 3^m)
+
+sumaProd :: Int -> Int -> Bool
+sumaProd 0 0 = True
+sumaProd n m = (productoSumas n m) == ((sumaQaLaN n 2)*(sumaQaLaN m 3))
