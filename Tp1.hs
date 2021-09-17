1fa90b7006832861657cd87427153c0abf818766
Marcos Attwell <marcos.attwell@gmail.com>
2021-09-17 17:23:37 -0300

    Agregue una version funcional de esSumaDeDosCubos a Tp1 y agregue casos de prueba al Test y documentacion de como usar el test.

--- Tp1.hs
+++ Tp1.hs
@@ -1,15 +1,24 @@
 module Tp1 where
+-- Funciones del TP1, archivo de marcos
 
 -- Funcion auxiliar
 esUnCubo :: Integer -> Bool
 esUnCubo x = (round (fromIntegral x**(1/3)))^3 == x
 
--- Funcion que indica si un numero es suma de dos cubos. N es a^3 + b^3 = N ?
+-- Funcion que indica si un numero es suma de dos cubos. N es tal que a^3 + b^3 = N
 esSumaDeDosCubos :: Integer -> Bool
-esSumaDeDosCubos n = False
+esSumaDeDosCubos n = esNSumaDeAHasta n 1 (mayorCuboAprox n)
 
+-- Funcion auxiliar, Le resta el cubo de A a N hasta encontrar un cubo B.
+esNSumaDeAHasta :: Integer -> Integer -> Integer -> Bool
+esNSumaDeAHasta n a mayorCubo | esUnCubo b && b >= 1 = True
+                              | a == mayorCubo       = False
+                              | otherwise            = esNSumaDeAHasta n (a+1) mayorCubo
+                                    where b = (n - a^3)
 
-
+-- Devuelve el numero mas grande que al cubo es menor a N. Ej mayorCuboAprox 7 = 1, 1^3 <= 7.  mayorCuboAprox 8 = 2, 2^3 <= 8
+mayorCuboAprox :: Integer -> Integer
+mayorCuboAprox n = (floor (fromIntegral n**(1/3)))
 
 descomposicionCubos :: Integer -> (Integer,Integer)
 descomposicionCubos n = (0,0)
--- testTP1.hs
+++ testTP1.hs
@@ -1,4 +1,11 @@
 import Tp1
+{-
+    Testea las funciones del archivo Tp1.hs (se le puede cambiar el nombre) 
+    * Tp1.hs tiene que estar en la misma carpeta que este archivo para compilarlo bien.
+    * Las funciones que no esten completas hay que definirlas con un valor predeterminado. Ej. cantidadDeFormas n = 0
+    Este archivo se compila con este comando en la consola: ghc testTP1.hs
+                                      y se ejecuta (Linux): ./testTP1
+-}
 
 -- Funciones de Testeo: toman una funcion la evaluan al segundo argumento y la comparan con el tercero. Si la prueba fue exitosa devuelven True.
 testFuncionBool :: (Integer -> Bool) -> Integer -> Bool -> Bool
@@ -14,10 +21,17 @@ main = do
     putStrLn "Programa para testear las funciones del TP1."
     putStrLn "--------1: Test esSumaDeDosCubos --------\n"
     putStrLn (show (testFuncionBool esSumaDeDosCubos 88 False))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 8 False))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 1 False))
     putStrLn (show (testFuncionBool esSumaDeDosCubos 2 True))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 7 False))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 9 True))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 16 True))
     putStrLn (show (testFuncionBool esSumaDeDosCubos 79625 True))
     putStrLn (show (testFuncionBool esSumaDeDosCubos 97187584969377 True))
     putStrLn (show (testFuncionBool esSumaDeDosCubos 97187584969378 False))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 35 True))
+    putStrLn (show (testFuncionBool esSumaDeDosCubos 34 False))
 
     putStrLn "--------2: Test descomposicionCubos --------\n"
     putStrLn (show (testFuncionParInteger descomposicionCubos 9 (1,2)))
@@ -44,12 +58,13 @@ main = do
     putStrLn (show (testFuncionInteger especialNumero 12 110808))
 
     putStrLn "--------6: Test esMuyEspecial --------\n"
-    putStrLn (show (testFuncionBool esSumaDeDosCubos 165464 False))
-    putStrLn (show (testFuncionBool esSumaDeDosCubos 1729 True))
-    putStrLn (show (testFuncionBool esSumaDeDosCubos 3 False))
-    putStrLn (show (testFuncionBool esSumaDeDosCubos 20683 True))
-    putStrLn (show (testFuncionBool esSumaDeDosCubos 805688 True))
+    putStrLn (show (testFuncionBool esMuyEspecial 165464 False))
+    putStrLn (show (testFuncionBool esMuyEspecial 1729 True))
+    putStrLn (show (testFuncionBool esMuyEspecial 3 False))
+    putStrLn (show (testFuncionBool esMuyEspecial 20683 True))
+    putStrLn (show (testFuncionBool esMuyEspecial 805688 True))
 
+    putStrLn "\n----- tocar alguna tecla para terminar -----\n"
     line <- getLine
     if null line
         then main
