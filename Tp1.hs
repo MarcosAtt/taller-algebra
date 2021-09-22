ee4ae591227389adb2f8e152e8970aa062852aeb
Marcos Attwell <marcos.attwell@gmail.com>
2021-09-22 19:56:19 -0300

    Saque parentesis de mas.

--- Tp1.hs
+++ Tp1.hs
@@ -3,22 +3,22 @@ module Tp1 where
 
 -- Funcion auxiliar
 esUnCubo :: Integer -> Bool
-esUnCubo x = (round (fromIntegral x**(1/3)))^3 == x
+esUnCubo x = round (fromIntegral x**(1/3))^3 == x
 
 -- Funcion que indica si un numero es suma de dos cubos. N es tal que a^3 + b^3 = N,  con N perteneciente a los numeros Naturales.
 esSumaDeDosCubos :: Integer -> Bool
-esSumaDeDosCubos n = not (n < 1) && esNSumaDeAHasta n 1 (mayorCuboAprox n)
+esSumaDeDosCubos n = n >= 1 && esNSumaDeAHasta n 1 (mayorCuboAprox n)
 
 -- Funcion auxiliar, Le resta el cubo de A a N hasta encontrar un cubo B.
 esNSumaDeAHasta :: Integer -> Integer -> Integer -> Bool
 esNSumaDeAHasta n a mayorCubo | esUnCubo b && b >= 1 = True
                               | a == mayorCubo       = False
                               | otherwise            = esNSumaDeAHasta n (a+1) mayorCubo
-                                    where b = (n - a^3)
+                                    where b = n - a^3
 
 -- Devuelve el numero mas grande que al cubo es menor a N. Ej mayorCuboAprox 7 = 1, 1^3 <= 7.  mayorCuboAprox 8 = 2, 2^3 <= 8
 mayorCuboAprox :: Integer -> Integer
-mayorCuboAprox n = (floor (fromIntegral n**(1/3)))
+mayorCuboAprox n = floor (fromIntegral n**(1/3))
 
 -- Toma un entero y devuelve un par ordenado tal que a^3 + b^3 = n. Codigo de esNSumaDeAHasta reciclado.
 descomposicionCubos :: Integer -> (Integer,Integer)
@@ -26,19 +26,19 @@ descomposicionCubos n | not (esSumaDeDosCubos n) = (0,0)
                       | otherwise = descomposicionCubosAux n 1 (mayorCuboAprox n)
 
 descomposicionCubosAux :: Integer -> Integer -> Integer -> (Integer,Integer)
-descomposicionCubosAux n a mayorCubo | esUnCubo b && b >= 1     = (a,(round (fromIntegral b**(1/3))))
+descomposicionCubosAux n a mayorCubo | esUnCubo b && b >= 1     = (a,round (fromIntegral b**(1/3)))
                                      | otherwise                = descomposicionCubosAux n (a+1) mayorCubo
-                                          where b = (n - a^3)
+                                          where b = n - a^3
 
 cantidadDeFormas :: Integer -> Integer
 cantidadDeFormas n | not (esSumaDeDosCubos n) = 0
-                   | otherwise                = (cantidadDeFormasAux n 1 (mayorCuboAprox n)) `div` 2 -- lo divido por 2 porque repite cada caso exactamente 2 veces.
+                   | otherwise                = cantidadDeFormasAux n 1 (mayorCuboAprox n) `div` 2 -- lo divido por 2 porque repite cada caso exactamente 2 veces.
 
 cantidadDeFormasAux :: Integer -> Integer -> Integer -> Integer
 cantidadDeFormasAux n a cota | a > cota = 0
                              | esUnCubo b && b >= 1 = 1 + cantidadDeFormasAux n (a+1) cota
                              | otherwise            =     cantidadDeFormasAux n (a+1) cota
-                                    where b = (n - a^3)
+                                    where b = n - a^3
 
 especialDesde :: Integer -> Integer
 especialDesde n = 0
