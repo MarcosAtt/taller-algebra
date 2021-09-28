08ec7b0d1e391d2c32e3d103129acb1e10b85244
Marcos Attwell <marcos.attwell@gmail.com>
2021-09-27 23:43:34 -0300

    Agregadas funciones: cantidadDeFormas (aux), especialDesde, especialNumero (aux), esMuyEspecial (aux).

--- Tp1.hs
+++ Tp1.hs
@@ -32,22 +32,40 @@ descomposicionCubosAux :: Integer -> Integer -> Integer -> (Integer,Integer)
 descomposicionCubosAux n a mayorCubo | esUnCubo b && b >= 1     = (a, raizCubica b)
                                      | otherwise                = descomposicionCubosAux n (a+1) mayorCubo
                                           where b = n - a^3
+
 -- 3 Devuelve la cantidad de formas de escribir
 cantidadDeFormas :: Integer -> Integer
 cantidadDeFormas n | not (esSumaDeDosCubos n) = 0
                    | otherwise                = cantidadDeFormasAux n 1 (mayorCuboAprox n) `div` 2 -- lo divido por 2 porque repite cada caso exactamente 2 veces.
 
 cantidadDeFormasAux :: Integer -> Integer -> Integer -> Integer
-cantidadDeFormasAux n a cota | a > cota = 0
+cantidadDeFormasAux n a cota | a > cota             = 0
                              | esUnCubo b && b >= 1 = 1 + cantidadDeFormasAux n (a+1) cota
                              | otherwise            =     cantidadDeFormasAux n (a+1) cota
                                     where b = n - a^3
 
+-- 4 - Siguiente numero especial desde n. (especial: cantidad de formas >= 2)
 especialDesde :: Integer -> Integer
-especialDesde n = 0
+especialDesde n | n <= 1729 = 1729
+                | cantidadDeFormas n >= 2 = n
+                | otherwise = especialDesde (n+1)
 
+-- 5 - N-esimo especial.
 especialNumero :: Integer -> Integer
-especialNumero n = 0
+especialNumero n = especialNumeroAux 1729 n
 
+especialNumeroAux :: Integer -> Integer -> Integer
+especialNumeroAux n c | c == 1 = n
+                      | otherwise = especialNumeroAux (especialDesde (n+1)) (c-1)
+
+-- 6 - Devuelve True si un numero especial no se puede escribir como producto de un cubo por otro especial.
 esMuyEspecial :: Integer -> Bool
-esMuyEspecial n = False
+esMuyEspecial n | (cantidadDeFormas n) < 2 = False
+                | otherwise = esMuyEspecialAux n 2 1729
+
+-- Funcion auxiliar que va a calcular si el numero n es multiplo de algun k^3 * m , siendo m un numero especial anterior a n.
+esMuyEspecialAux :: Integer -> Integer -> Integer -> Bool
+esMuyEspecialAux n k m | n == (k^3) * m = False
+                       | n >  (k^3) * m = esMuyEspecialAux n (k+1) m 
+                       | m >= n = True
+                       | otherwise = esMuyEspecialAux n 2 (especialDesde (m+1)) 
