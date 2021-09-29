435153d3226e4054188cc1068a0ec222a43a4e35
Marcos Attwell <marcos.attwell@gmail.com>
2021-09-29 16:31:29 -0300

    Clase 6 listas. Funciones completadas

--- Clase06.hs
+++ Clase06.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Clase06 where
 -- Clase sobre listas
 sumatoria :: [Int] -> Int
@@ -25,11 +26,6 @@ primerMultiplode45345 [] = 0
 primerMultiplode45345 l | mod (head l) 45345 == 0 = (head l)
                         | otherwise = primerMultiplode45345 (tail l)
 
-buscarElementosIguales :: (Eq a)=> [a] -> Int
-buscarElementosIguales [] = 0
-buscarElementosIguales l  | (head l) == (head (tail l)) = 1 + buscarElementosIguales (tail l)
-                          | otherwise = 1
-
 comprimir :: (Eq a)=> [a] -> [(a,Int)]
 comprimir (x:xs) | xs == []     = [(x,1)]
                  | x /= head xs = (x,1):comprimir (xs)
@@ -43,3 +39,95 @@ agregarNvecesAlprincipio n e l = e:(agregarNvecesAlprincipio (n-1) e l)
 -- p = (e,n)
 descomprimir [] = []
 descomprimir (p:ps) = agregarNvecesAlprincipio (snd p) (fst p) (descomprimir ps)
+
+productoria :: [Int] -> Int
+productoria [] = 0
+productoria (x:xs) | xs == [] = x
+                   | otherwise = x + productoria xs
+
+sumarN :: Int -> [Int] -> [Int]
+sumarN n (x:xs) | xs == [] = [(x+n)]
+                | otherwise = (x+n):(sumarN n (xs))
+
+sumarElPrimero :: [Int] -> [Int]
+sumarElPrimero (x:xs) = sumarN (x) (xs)
+
+ultimo :: [Int] -> Int
+ultimo [] = 0
+ultimo (x:xs) | xs == [] = x
+              | otherwise = ultimo xs
+
+sumarElUltimo :: [Int] -> [Int]
+sumarElUltimo (x:xs) = sumarN (ultimo xs) (x:xs)
+
+pares :: [Int] -> [Int]
+pares [] = []
+pares (x:xs) | x `mod` 2 == 0 = x:(pares xs)
+             | otherwise = pares xs
+
+quitar :: Int -> [Int] -> [Int]
+quitar n (x:xs) | xs == [] && x /= n = [x]
+                | xs == [] = []
+                | x == n = sumarN 0 (xs)
+                | otherwise = (x):(quitar n (xs))
+
+quitarTodas :: Int -> [Int] -> [Int]
+quitarTodas _ [] = []
+quitarTodas n (x:xs) | xs == [] = []
+                     | pertenece n (x:xs) = quitarTodas n (quitar n (x:xs))
+                     | otherwise = x:xs
+
+hayRepetidos :: [Int] -> Bool
+hayRepetidos (x:xs) | xs == [] = False
+                    | pertenece x xs = True
+                    | otherwise = hayRepetidos xs
+
+eliminarRepetidosAlFinal :: [Int] -> [Int]
+eliminarRepetidosAlFinal [] = []
+eliminarRepetidosAlFinal (x:xs) | xs == [] = [x]
+                                | hayRepetidos (x:xs) = x:(eliminarRepetidosAlFinal (quitarTodas x xs))
+                                | otherwise = x:xs
+
+eliminarRepetidosAlInicio :: [Int] -> [Int]
+eliminarRepetidosAlInicio (x:xs) | xs == [] = [x]
+                                 | pertenece x xs = eliminarRepetidosAlInicio xs
+                                 | otherwise      = x:(eliminarRepetidosAlInicio xs)
+
+maximo :: [Int] -> Int
+maximo (x:xs) = maximoAux (eliminarRepetidosAlFinal (x:xs))
+
+maximoAux :: [Int] -> Int
+maximoAux (x:xs) | xs == [] = x
+                 | x < (head xs) = maximoAux xs
+                 | otherwise = maximoAux (x:(tail xs))
+
+minimo :: [Int] -> Int
+minimo (x:xs) = minimoAux (eliminarRepetidosAlFinal (x:xs))
+
+minimoAux :: [Int] -> Int
+minimoAux (x:xs) | xs == [] = x
+                 | x > (head xs) = minimoAux xs
+                 | otherwise = minimoAux (x:(tail xs))
+
+ordenar :: [Int] -> [Int]
+ordenar l | l == [] = []
+          | otherwise = (minimo l):(ordenar (quitar (minimo l) l))
+
+sinUltimo :: [Int] -> [Int]
+sinUltimo [] = []
+sinUltimo (x:xs) | xs == [] = []
+                 | otherwise = x:(sinUltimo xs)
+
+reverso :: [Int] -> [Int]
+reverso l | l == [] = []
+          | otherwise = (ultimo l):(reverso (sinUltimo l))
+
+concatenar :: [Int] -> [Int] -> [Int]
+concatenar _ [] = []
+concatenar a b | a == []   = (head b):(concatenar a (tail b))
+               | otherwise = (head a):(concatenar (tail a) b)
+
+zipi :: [a] -> [b] -> [(a,b)]
+zipi [] b = []
+zipi a [] = []
+zipi a b = ((head a),(head b)):(zipi (tail a) (tail b))
