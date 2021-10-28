c27e91df6e97d1d3a0380b32ce602dd3bf98dcc1
Marcos Attwell <marcos.attwell@gmail.com>
2021-10-27 23:40:54 -0300

    Primera version de Aritmetica.hs

--- Aritmetica.hs
+++ Aritmetica.hs
@@ -5,23 +5,74 @@ import Data.Bits
 
 
 --(1)
-mcdExt :: Integer -> Integer -> (Integer, (Integer, Integer))
-mcdExt _ _ = (0, (0, 0))
+emcd :: Int -> Int -> (Int, Int, Int)
+emcd 0 b = (b, 0, 1)
+emcd a b = (g, t - (b `div` a) * s, s)
+        where (g, s, t) = emcd (b `mod` a) a
 
 --(2)
-criba :: Integer -> Set Integer
-criba _ = []
+siguienteNumeroMayorN :: Set Integer -> Integer -> Integer
+siguienteNumeroMayorN [] n = n
+siguienteNumeroMayorN (x:xs) n | x > n = x
+                               | otherwise = siguienteNumeroMayorN xs n
 
+eliminarMultiplosLista :: Set Integer -> Integer -> Set Integer
+eliminarMultiplosLista [] _ = []
+eliminarMultiplosLista (x:xs) n | (x `mod` n == 0) && (x > n) = eliminarMultiplosLista xs n
+                                | otherwise = x:(eliminarMultiplosLista xs n)
+
+cribaAux :: Set Integer -> Integer -> Set Integer
+cribaAux l n | proximoPrimo == n = l
+             | otherwise = cribaAux (eliminarMultiplosLista l proximoPrimo) proximoPrimo 
+             where proximoPrimo = siguienteNumeroMayorN l n
+
+criba :: Integer -> Set Integer
+criba n = cribaAux [2..n] 1
+
+---------------- ESTO NO ES PARTE DEL TP SOLO TESTEO----------------------
+{-
+menorDivisor :: Integer -> Integer
+menorDivisor n = menorDivisorDesde n 2
+
+menorDivisorDesde :: Integer -> Integer -> Integer
+menorDivisorDesde n k | n == k = n
+                      | n `mod` k == 0 = k
+                      | otherwise = menorDivisorDesde n (k+1)
+
+esPrimo :: Integer -> Bool
+esPrimo 1 = False
+esPrimo n = n == menorDivisor n
+sonTODOSPRIMOS :: Set Integer -> Bool
+sonTODOSPRIMOS [] = True
+sonTODOSPRIMOS (x:xs) | not (esPrimo x) = False
+                      | otherwise = sonTODOSPRIMOS xs
+-}
+-----------------------------------------------------------------------
 --(3)
+mcd :: Integer -> Integer -> Integer
+mcd a 0 = abs a
+mcd 0 b = abs b
+mcd a b | (a == 1) || (b == 1) = 1
+        | abs a > abs b = mcd (a `mod` b) b
+        | abs b > abs a = mcd (b `mod` a) a
+
 coprimoCon:: Integer -> Integer
-coprimoCon _ = 0
+coprimoCon n = coprimoConAux n 2
+
+coprimoConAux :: Integer -> Integer -> Integer
+coprimoConAux n i | sonCoprimos n i = i
+                  | otherwise = coprimoConAux n (i+1)
+
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos a b = mcd a b == 1
 
 
 --(4)
 inversoMultiplicativo:: Integer -> Integer -> Integer
-inversoMultiplicativo _ _ = 0
+inversoMultiplicativo n base = inversoMultiplicativoAux n 1 base
 
-
+inversoMultiplicativoAux n k base | ((n*k) `mod` base) /= 1    = inversoMultiplicativoAux n (k+1) base
+                                  | otherwise                  = k
 
 -- Función de regalo para exponenciar "rápido"
 modExp :: Integer -> Integer -> Integer -> Integer
