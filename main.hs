409d6bac3bd284a661c504043d39c5c0c6ed8f8a
Marcos Attwell <marcos.attwell@gmail.com>
2021-10-30 00:21:02 -0300

    mejora de velocidad usan modExp

--- main.hs
+++ main.hs
@@ -17,7 +17,7 @@ magiaPura [] = []
 magiaPura (x:xs) = res : magiaPura xs
   where
     res = "Codificando " ++ show x ++ "\nEl cifrado es " ++ show cif
-            ++ "\nBob decodifico " ++ show msg ++ "\n" -- ++ dbg -- si le dejamos esto manda informacion de debuggeo
+            ++ "\nBob decodifico " ++ show msg ++ "\n" ++ dbg -- si le dejamos esto manda informacion de debuggeo
     dbg = "[DBG] Mis claves son " ++ show (e,d,n) ++ "\n[DBG] Mis primos " ++ show (p,q) ++ "\n"
     (p, q) = elegidor  $ criba 5000
     (e, d, n) = claves p q
